705d7efe925f64fa6597a6c49991b6d5db012c77
mcieciora <michalcieciora96@gmail.com>
2022-05-13 21:20:47 +0200

    13052020-2120

--- src/mongodb.py
+++ src/mongodb.py
@@ -3,12 +3,11 @@ from pymongo import MongoClient
 
 class MongoDb:
     def __init__(self):
-        pass
+        self.client = MongoClient(host='mongodb', port=27017)
+        self.db = self.client['burly_pronghorn']
+        self.main = self.db['main']
 
     def insert(self, data):
-        pass
-
-    def update(self, query, data):
         pass
 
     def find(self, query):
