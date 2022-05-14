7a829deb48113818baca7abe53356de28720f405
mcieciora <michalcieciora96@gmail.com>
2022-05-14 21:47:07 +0200

    14052022-2147

--- src/api.py
+++ src/api.py
@@ -4,19 +4,36 @@ from mongodb import MongoDb
 
 @route('/<request_type>')
 def index(request_type):
-    pass
+    requests = {
+        'api': Api,
+        'insert': Insert,
+        'find': Find,
+        'delete': Delete
+    }
+    payload = request.query.dict
+    if verify_payload(request.query.dict):
+        request_object = requests[request_type](payload)
+        request_object.action()
 
 
-def verify_payload():
-    pass
+def verify_payload(actual_dict):
+    return_value = True
+    expected_keys = ['object_name', 'note', 'related_tasks', 'active_days']
+    if len(expected_keys) != len(actual_dict):
+        return_value = False
+    if sorted(actual_dict) != sorted(expected_keys):
+        return_value = False
+    return return_value
 
 
 class Api:
     def __init__(self, payload_dict):
         self.payload_dict = payload_dict
+        for key, value in self.payload_dict.items():
+            self.payload_dict[key] = value[0]
 
     def action(self):
-        pass
+        return dict(data=[{"status": 'OK'}])
 
 
 class Find(Api):
@@ -43,4 +60,4 @@ class Delete(Api):
         pass
 
 
-run(host='localhost', port=8000)
+run(host='localhost', port=7999)
