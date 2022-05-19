70215169ceb50aaebc4414a4c1332231d004eec5
mcieciora <michalcieciora96@gmail.com>
2022-05-19 18:01:37 +0200

    19052022-1801

--- automated_tests/test_api_find.py
+++ automated_tests/test_api_find.py
@@ -5,6 +5,6 @@ from requests import get
 @mark.unittest
 def test__unit__basic_query(mongo_database_one_record):
     query = {'object_name': 'test_name'}
-    return_data = get('http://localhost:7999/find', params=query)
+    return_data = get('http://0.0.0.0:7999/find', params=query)
     assert return_data.status_code == 200, f'Status code is not 200: {return_data.reason}'
     print(return_data.content)
