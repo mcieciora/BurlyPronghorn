b65c0bf7e6cbecc180cb79b10da683cf438a028c
mcieciora <michalcieciora96@gmail.com>
2022-05-20 14:14:52 +0200

    20052022-1414

---
+++ automated_tests/unittests/test_api.py
@@ -0,0 +1,13 @@
+from pytest import mark
+
+
+@mark.skip
+@mark.regular
+def test__regular__insert_find_delete_record():
+    pass
+
+
+@mark.skip
+@mark.regular
+def test__regular__insert_value_two_times():
+    pass
