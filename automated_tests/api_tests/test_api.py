faf4951fc535a275846709191a23343d59e952be
mcieciora <michalcieciora96@gmail.com>
2022-05-21 19:52:33 +0200

    21052022-1952

--- automated_tests/api_tests/test_api.py
+++ automated_tests/api_tests/test_api.py
@@ -1,13 +1,20 @@
 from pytest import mark
+from requests import get
 
 
-@mark.skip
 @mark.regular
 def test__regular__insert_find_delete_record():
-    pass
-
-
-@mark.skip
-@mark.regular
-def test__regular__insert_value_two_times():
-    pass
+    test_data = {'object_name': 'test_name', 'note': 'example_note', 'related_tasks': 'NaN', 'active_days': '0'}
+    return_data = get('http://0.0.0.0:7999/insert', params=test_data)
+    assert return_data.status_code == 200, f'Status code is not 200: {return_data.reason}'
+    query = {'object_name': 'test_name'}
+    return_data = get('http://0.0.0.0:7999/find', params=query)
+    assert return_data.status_code == 200, f'Status code is not 200: {return_data.reason}'
+    assert '{"data": [{"object_name": "test_name", "note": "example_note", "related_tasks": "NaN", ' \
+           '"active_days": "0"}' in str(return_data.content), return_data.content
+    return_data = get('http://0.0.0.0:7999/delete', params=query)
+    assert return_data.status_code == 200, f'Status code is not 200: {return_data.reason}'
+    assert '{"status": "OK"}]}' in str(return_data.content), return_data.content
+    return_data = get('http://0.0.0.0:7999/find', params=query)
+    assert return_data.status_code == 200, f'Status code is not 200: {return_data.reason}'
+    assert '{"data": []}' in str(return_data.content), return_data.content
