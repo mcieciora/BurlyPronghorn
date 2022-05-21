b61bb6fe364b2b011361a11d60684b7954ea00d2
mcieciora <michalcieciora96@gmail.com>
2022-05-21 22:44:52 +0200

    21052022-2244

--- src/mongodb.py
+++ src/mongodb.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 
 class MongoDb:
     def __init__(self):
-        self.client = MongoClient(host='localhost', port=27017)
+        self.client = MongoClient(host='mongodb', port=27017)
         self.db = self.client['burly_pronghorn']
         self.main = self.db['main']
 
