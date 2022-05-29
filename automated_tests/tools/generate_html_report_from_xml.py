7ab54d95804f4c9918b959958d822d0375efb15e
mcieciora <michalcieciora96@gmail.com>
2022-05-29 14:51:55 +0200

    29052022-1451

--- automated_tests/tools/generate_html_report_from_xml.py
+++ automated_tests/tools/generate_html_report_from_xml.py
@@ -2,10 +2,11 @@ from jinja2 import Environment, FileSystemLoader
 import xml.etree.ElementTree as ET
 from glob import glob
 from datetime import datetime
+from os import getcwd
 
 
 def generate_html(date, number_of_tests, tests):
-    env = Environment(loader=FileSystemLoader('.'))
+    env = Environment(loader=FileSystemLoader(getcwd()))
     template = env.get_template('html_report_template.html')
 
     with open('results.html', 'w') as f:
